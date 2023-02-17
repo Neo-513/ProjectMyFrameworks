c4d2978949f68bf08e038bdf9fcfeef0e2eb8e54
Neo-0513 <1829563047@qq.com>
2023-02-17 10:05:02 +0800

    Initial commit

--- myframeworks/myparser.py
+++ myframeworks/myparser.py
@@ -1,19 +1,19 @@
-table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
-orders = [11, 10, 3, 8, 4, 6]
-xor = 177451812
-offset = 8728348608
+_TABLE = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
+_ORDERS = [11, 10, 3, 8, 4, 6]
+_XOR = 177451812
+_OFFSET = 8728348608
 
 
 def bv2av(bv):
-	dic = {t: i for i, t in enumerate(table)}
-	items = ((dic[bv[order]] * len(table) ** i) for i, order in enumerate(orders))
-	return (sum(items) - offset) ^ xor
+	dic = {t: i for i, t in enumerate(_TABLE)}
+	items = ((dic[bv[order]] * len(_TABLE) ** i) for i, order in enumerate(_ORDERS))
+	return (sum(items) - _OFFSET) ^ _XOR
 
 
 def av2bv(av):
-	bv = (av ^ xor) + offset
+	bv = (av ^ _XOR) + _OFFSET
 	mask = list("BV1  4 1 7  ")
-	for i, order in enumerate(orders):
-		const = len(table) ** i
-		mask[order] = table[bv // const % len(table)]
+	for i, order in enumerate(_ORDERS):
+		const = len(_TABLE) ** i
+		mask[order] = _TABLE[bv // const % len(_TABLE)]
 	return "".join(mask)
