968c5390d65b8a1af50726afe665eef8bcf2e89b
Neo-0513 <1829563047@qq.com>
2023-02-19 07:55:44 +0800

    Initial commit

--- myframeworks/mycrawler.py
+++ myframeworks/mycrawler.py
@@ -11,8 +11,8 @@ import re
 import time
 
 HEADERS = {
-	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0"
-	" Safari/537.36 Edg/107.0.1418.42"
+	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0"
+	" Safari/537.36 Edg/110.0.1587.49"
 }  # 请求头
 
 
