001fcd8bb2f8071911c2bb2f9c2f6f8a90701c53
Neo-0513 <1829563047@qq.com>
2023-02-20 14:19:10 +0800

    Initial commit

--- call_cmd.py
+++ call_cmd.py
@@ -1,8 +1,8 @@
 import os
 
 commands = [
-	"D:",
-	"cd D:/workspace_PyCharm/ProjectMyFrameworks",
+	"d:",
+	"cd d:/workspace_PyCharm/ProjectMyFrameworks",
 	"python setup.py sdist",
 	"pip install dist/myframeworks-0.1.tar.gz"
 ]
